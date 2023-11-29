54d5d60e344a9d30aaf1c2bb762aed6e28a782ed
Rakesh <122853561+rakhz@users.noreply.github.com>
2023-11-29 08:47:21 +0530

    Add files via upload

---
+++ Skeleton code for hotel management system.py
@@ -0,0 +1,60 @@
+# coding=System
+from Rooms import *
+
+class Customer(object):
+
+  """
+   
+
+  :version:
+  :author:
+  """
+
+  """ ATTRIBUTES
+
+   
+
+  Customer_name  (private)
+
+   
+
+  Email  (private)
+
+   
+
+  Room_Booking  (private)
+
+   
+
+  Address  (private)
+
+   
+
+  Due_date  (private)
+
+   
+
+  Booking  (private)
+
+  """
+
+  def Update(self):
+    """
+     
+
+    @return  :
+    @author
+    """
+    pass
+
+  def Room_availability(self):
+    """
+     
+
+    @return  :
+    @author
+    """
+    pass
+
+
+
