afbb2073c14587e014c9bc937e22e90155390f69
Rakesh <122853561+rakhz@users.noreply.github.com>
2023-11-30 10:02:36 +0530

    Add files via upload

---
+++ Skeleton code for Course registration.py
@@ -0,0 +1,34 @@
+# coding=System
+
+class Registration(object):
+
+  """
+   
+
+  :version:
+  :author:
+  """
+
+  """ ATTRIBUTES
+
+   
+
+  Reg_No_  (private)
+
+   
+
+  Course_opted  (private)
+
+  """
+
+  def Display(self):
+    """
+     
+
+    @return  :
+    @author
+    """
+    pass
+
+
+
---
+++ Skeleton diagram for foregin trading system.py
@@ -0,0 +1,76 @@
+# coding=System
+from ADMIN import *
+from ITEM import *
+
+class TRADER (ADMIN, ITEM):
+
+  """
+   
+
+  :version:
+  :author:
+  """
+
+  """ ATTRIBUTES
+
+   
+
+  User_name  (private)
+
+   
+
+  Password  (private)
+
+   
+
+  Account_no_  (private)
+
+  """
+
+  def Login(self):
+    """
+     
+
+    @return  :
+    @author
+    """
+    pass
+
+  def Sourcing(self):
+    """
+     
+
+    @return  :
+    @author
+    """
+    pass
+
+  def Place_order(self):
+    """
+     
+
+    @return  :
+    @author
+    """
+    pass
+
+  def Pay(self):
+    """
+     
+
+    @return  :
+    @author
+    """
+    pass
+
+  def Logout(self):
+    """
+     
+
+    @return  :
+    @author
+    """
+    pass
+
+
+
